84b8e4d9ba4b84bba465cda8c49ee17f6cf4bba7
Luke Yin <yinluke@gmail.com>
2020-11-20 17:38:51 -0400

    new project

---
+++ iOSGameHelper/iOSGameHelper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  iOSGameHelper
+//
+//  Created by Presentation on 2020-11-20.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ iOSGameHelper/iOSGameHelper/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  iOSGameHelper
+//
+//  Created by Presentation on 2020-11-20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
